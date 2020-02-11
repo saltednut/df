901761cfb6f736c1a7671137e2f35a0158e2d129
Brant Wynn <brant.wynn@acquia.com>
2020-02-11 11:44:47 -0600

    Remove Lightning Dev (#204)
    Removes Lightning Dev, consolidates testing into DF.

--- RoboFile.php
+++ RoboFile.php
@@ -1,13 +1,24 @@
 <?php
 
+use Symfony\Component\Finder\Finder;
+use Symfony\Component\Yaml\Yaml;
+use Drupal\Component\Utility\NestedArray;
+
 class RoboFile extends \Robo\Tasks {
+
+  /**
+   * The default URL of the Drupal site.
+   *
+   * @var string
+   */
+  const BASE_URL = 'http://127.0.0.1:8080';
 
   /**
    * {@inheritdoc}
    */
   protected function taskBehat($behat = NULL) {
     return parent::taskBehat($behat ?: 'vendor/bin/behat')
-      ->config('docroot/sites/default/files/behat.yml')
+      ->config('.behat.yml')
       ->format('pretty')
       ->option('colors')
       ->option('stop-on-failure')
@@ -24,6 +35,46 @@ class RoboFile extends \Robo\Tasks {
     return $this->taskExec($drush ?: '../vendor/bin/drush')
       ->rawArg($command)
       ->dir('docroot');
+  }
+
+  /**
+   * Configures Behat.
+   *
+   * @param string $base_url
+   *  (optional) The URL of the Drupal site.
+   *
+   * @return \Robo\Contract\TaskInterface
+   *   The task to execute.
+   */
+  public function configureBehat ($base_url = NULL) {
+    $configuration = [];
+
+    /** @var Finder $partials */
+    $partials = Finder::create()->in('docroot')->files()->name('behat.partial.yml');
+
+    foreach ($partials as $partial) {
+      $partial = str_replace(
+        '%paths.base%',
+        $partial->getPathInfo()->getRealPath(),
+        $partial->getContents()
+      );
+      $configuration = NestedArray::mergeDeep($configuration, Yaml::parse($partial));
+    }
+
+    if ($configuration) {
+      $configuration = str_replace([
+          '%base_url%',
+          '%drupal_root%',
+        ],
+        [
+          $base_url ?: static::BASE_URL,
+          'docroot',
+        ],
+        Yaml::dump($configuration)
+      );
+      return $this->taskWriteToFile('.behat.yml')->text($configuration);
+    }
+
   }
 
   /**
