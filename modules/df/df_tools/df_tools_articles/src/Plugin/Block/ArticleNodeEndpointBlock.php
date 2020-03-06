7868ac43b623803ad7ec59ea3da94e5f64d3af2c
Brant Wynn <brant.wynn@acquia.com>
2020-03-06 14:32:10 -0600

    Adds the Article Node Endpoint block

---
+++ modules/df/df_tools/df_tools_articles/src/Plugin/Block/ArticleNodeEndpointBlock.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Drupal\df_tools_articles\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use GuzzleHttp\Client;
+
+/**
+ * Provides a 'ArticleNodeEndpointBlock' block.
+ *
+ * @Block(
+ *  id = "article_node_endpoint_block",
+ *  admin_label = @Translation("Article node endpoint block"),
+ *  category = "Lists",
+ * )
+ */
+class ArticleNodeEndpointBlock extends BlockBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * GuzzleHttp\Client definition.
+   *
+   * @var \GuzzleHttp\Client
+   */
+  protected $httpClient;
+
+  /**
+   * CollectionNodeEndpointBlock constructor.
+   * @param array $configuration
+   * @param string $plugin_id
+   * @param mixed $plugin_definition
+   * @param \GuzzleHttp\Client $http_client
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, Client $http_client) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->httpClient = $http_client;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('http_client')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    $build = array();
+    global $base_url;
+    header('Content-Type: application/json');
+    $json_output = (string) $this->httpClient->get($base_url . '/api/node/article')->getBody();
+    $json_pretty = json_encode(json_decode($json_output), JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
+    $json_indented_by_2 = preg_replace('/^(  +?)\\1(?=[^ ])/m', '$1', $json_pretty);
+    $build['article_node_endpoint_block']['#markup'] = '<a data-toggle="language-json" class="button small">Expand Code</a> <div id="language-json" class="api-demo" data-toggler=".expanded"><pre><code class="language-json">' . $json_indented_by_2 . '</code></pre></div>';
+    $build['article_node_endpoint_block']['#attached']['library'][] = 'df_tools_articles/main';
+    return $build;
+  }
+
+}
