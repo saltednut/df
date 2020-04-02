5e20aeff1e5cf0bb97288ec5f9c9c14a16dc4b4e
Kevin Funk <kevin.funk@acquia.com>
2020-04-02 14:13:38 -0400

    Adds modal for api demo.

--- modules/df/df_tools/df_tools_articles/src/Plugin/Block/ArticleNodeEndpointBlock.php
+++ modules/df/df_tools/df_tools_articles/src/Plugin/Block/ArticleNodeEndpointBlock.php
@@ -59,8 +59,7 @@ class ArticleNodeEndpointBlock extends BlockBase implements ContainerFactoryPlug
     $json_output = (string) $this->httpClient->get($base_url . '/api/node/article')->getBody();
     $json_pretty = json_encode(json_decode($json_output), JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
     $json_indented_by_2 = preg_replace('/^(  +?)\\1(?=[^ ])/m', '$1', $json_pretty);
-    $build['article_node_endpoint_block']['#markup'] = '<a class="btn btn-primary" data-toggle="collapse" href="#collapseCode" role="button" aria-expanded="false" aria-controls="collapseExample">Show/Hide API Response</a>
-    <div class="collapse.show" id="collapseCode"><pre><code class="language-json">' . $json_indented_by_2 . '</code></pre></div>';
+    $build['article_node_endpoint_block']['#markup'] = '<a class="btn btn-primary" data-toggle="modal" data-target="#apiModal" href="#open" role="button" aria-expanded="false" aria-controls="collapseExample">Expand API Response</a><div class="apiResponse"><pre><code class="language-json">' . $json_indented_by_2 . '</code></pre></div><div class="modal" id="apiModal"><div class="modal-dialog modal-dialog-centered"><div class="modal-content"><div class="modal-header"><h1 class="modal-title">API Response</h1><a type="button" class="close" data-dismiss="modal" href="#close">×</a></div><pre><code class="language-json">' . $json_indented_by_2 . '</code></pre><div class="modal-footer"><a type="button" class="btn btn-danger" data-dismiss="modal" href="#close">Close</a></div></div></div></div>';
     $build['article_node_endpoint_block']['#attached']['library'][] = 'df_tools_articles/main';
     return $build;
   }
